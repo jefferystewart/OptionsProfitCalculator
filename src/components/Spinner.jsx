dee3978f32fe9f4b2ece0b9018848e15f69511de
jefferystewart <jefferystewart933@gmail.com>
2021-06-29 20:09:14 -0300

    Spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+export const Spinner = () => {
+  return (
+    <div className="text-center my-5">
+      <div className="spinner-border text-light" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  )
+}
