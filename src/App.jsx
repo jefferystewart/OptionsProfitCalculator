ee2223d82fa71c47153d30881b19372bc1222109
jefferystewart <jefferystewart933@gmail.com>
2021-06-29 16:54:24 -0300

    IN PROGRESS -- Trending component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
+import { Trending } from './components/Trending'
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Trending />
       <Footer />
     </>
   )
