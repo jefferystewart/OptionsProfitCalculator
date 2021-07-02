cd7b86e141afa60cfb6a5f9f676a025d31a225fc
jefferystewart <jefferystewart933@gmail.com>
2021-07-01 21:36:02 -0300

    Remove bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
+
 import { App } from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
