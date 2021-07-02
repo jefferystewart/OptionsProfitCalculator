320be47874562e00ba7e3cecb011bf8e56138059
jefferystewart <jefferystewart933@gmail.com>
2021-07-02 17:06:28 -0300

    Hero component for show a description website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import Logo from '../assets/logo.svg'
+import { Hero } from './Hero'
 
 export const Header = () => {
   return (
+    <>
     <nav className="navbar">
         <a className="navbar__logo-link navbar--header" href="#">
           <img src={Logo} className="navbar__logo-img" />
           <h1 className="navbar__logo-text">React Fintech Cryptocurrency</h1>
         </a>
     </nav>
+    <Hero />
+    </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="hero__title">
+        In an age where the economy is digital.
+      </h1>
+      <h1 className="hero__title">
+        Know what happens with vital cryptocurrencies.
+      </h1>
+      <h2 className="hero__text">
+        A cryptocurrency is a form of digital asset based on a network 
+        that is distributed across a large number of computers. 
+        This decentralized structure allows them to exist outside the 
+        control of governments and central authorities. 
+      </h2>
+    </div>
+  )
+}
