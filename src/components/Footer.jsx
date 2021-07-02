77b5e9d21c75b56ccf9fcb5dcf10bc6d45b5dc4e
jefferystewart <jefferystewart933@gmail.com>
2021-07-02 20:40:15 -0300

    Add -- powered by -- on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,23 @@ import Logo from '../assets/logo.svg'
 
 export const Footer = () => {
   return (
-    <nav className="navbar">
-        <a className="navbar__logo-link navbar--footer" href="#">
-          <img src={Logo} className="navbar__logo-img" />
-          <h1 className="navbar__logo-text">RFC</h1>
+    <>
+      <nav className="navbar">
+          <a className="navbar__logo-link navbar--footer" href="#">
+            <img src={Logo} className="navbar__logo-img" />
+            <h1 className="navbar__logo-text">RFC</h1>
+          </a>
+      </nav>
+      <p className="powered">
+        Powered by 
+        <a 
+          href="https://linkedin.com/in/eanappi"
+          className="powered__link" 
+          target="_blank"
+        >
+        @eanappi
         </a>
-    </nav>
+      </p>
+    </>
   )
 }
