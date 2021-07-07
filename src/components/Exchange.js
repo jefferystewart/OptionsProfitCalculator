5fe13bfab5915d9949d5b24441a2486936620b2e
jefferystewart <jefferystewart933@gmail.com>
2021-07-07 10:14:48 -0300

    Modify styles and add exchange component

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -1,6 +1,6 @@
 import SupportedVsCurrencies from '../assets/supported_vs_currencies.json'
-import {useTrending} from '../hooks/useTrending'
-import {Spinner} from './Spinner'
+import { useTrending } from '../hooks/useTrending'
+import { Spinner } from './Spinner'
 
 export const Exchange = () => {
   // https://api.coingecko.com/api/v3/simple/price?ids=melon&vs_currencies=eur
@@ -8,21 +8,21 @@ export const Exchange = () => {
   if (isLoading) return <Spinner />
 
   return (
-    <>
-      <h1>Exchange ...</h1>
-      <select>
-        <option value={null}>Choose a value</option>
-        {SupportedVsCurrencies.map(currency => (
-          <option key={currency} value={currency}>{currency}</option>
-        ))}
-      </select>
-      <select>
-        <option value={null}>Choose a value</option>
+    <div className="exchange">
+      <select className="exchange__crypto">
+        <option className="exchange__crypto-option" value={null}>Choose a value</option>
         {data.coins.map(coin => (
-          <option key={coin.item.id} value={coin.item.id}>{coin.item.name}</option>
+          <option key={coin.item.id} className="exchange__crypto-option" value={coin.item.id}>{coin.item.name}</option>
         ))}
       </select>
-
-    </>
+      <select className="exchange__currency">
+        <option className="exchange__currency-option" value={null}>Choose a value</option>
+        {SupportedVsCurrencies.map(currency => (
+          <option key={currency} className="exchange__currency-option" value={currency}>{currency}</option>
+        ))}
+      </select>
+      <label>Lol</label>
+      <button className="exchange__button" ></button>
+    </div>
   )
 }
